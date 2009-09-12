9461df21f4c83e45c3f1cd9ff43af86cb5265b96
Anthony Underwood <email2ants@gmail.com>
2009-09-12 14:22:48 +0100

    add clearance

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,10 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "thoughtbot-clearance",
+  :lib     => 'clearance',
+  :source  => 'http://gems.github.com',
+  :version => '0.8.2'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -39,3 +43,4 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+DO_NOT_REPLY = "email2ants@gmail.com"
